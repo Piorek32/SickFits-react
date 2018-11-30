4c3ee6854c8c5352c5ef585e80d62662239f78fb
Piorek32 <piotr.obrebowski@gmail.com>
2018-11-30 12:41:52 +0100

    first commit

---
+++ frontend/components/Nav.js
@@ -0,0 +1,15 @@
+import  Link from 'next/link'
+
+
+
+const  Nav = () => (
+    <div>
+        <Link href='/sell'><a>sell</a></Link>
+
+        <Link href='/index'><a>home</a></Link>
+
+    </div>
+)
+
+
+export default Nav;
---
+++ frontend/pages/index.js
@@ -0,0 +1,17 @@
+import Link from 'next/link'
+
+
+
+
+
+
+
+
+const  Home = () => (
+    <div>
+    <p>home</p>
+    </div>
+)
+
+
+export default Home;
